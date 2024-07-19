2f919b51192ccf1643c6e3e31821f76aad9957bb
slidingman <pssy5xfd@gmail.com>
2024-07-20 02:38:11 +0900

    build時のエラー等を少し直す

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
+  console.log(req); //どこかで使う
   res.status(200).json({ message: "Hello from Vercel Serverless Function written in TypeScript!" });
 }
--- src/components/CreatePostButton.tsx
+++ src/components/CreatePostButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 const CreatePostButton = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
