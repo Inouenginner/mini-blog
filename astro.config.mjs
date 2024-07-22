408e96b5c40b0e24d15022768276fb8b9528687e
slidingman <pssy5xfd@gmail.com>
2024-07-22 18:05:27 +0900

    cleanUrlsを使用,adapter:vercel()を一度消す

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import vercel from '@astrojs/vercel';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind({
     applyBaseStyles: false,
-  })],
-  adapter:vercel()
+  })]
 });
